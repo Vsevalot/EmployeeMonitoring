01b32d0f3ae5b86f7af9323abd00cc8ebadf7133
vsevalot <v.v.tetervak@gmail.com>
2023-11-26 16:09:52 +0100

    cors headers

--- back/ports/api/v1/bootstrap.py
+++ back/ports/api/v1/bootstrap.py
@@ -20,6 +20,8 @@ def get_application(db_config: DBConfig) -> Application:
         "http://localhost:8080",
         "http://127.0.0.1",
         "http://127.0.0.1:8080",
+        "http://159.223.224.135"
+        "http://159.223.224.135:8080"
     ]
 
     app.add_middleware(
@@ -28,6 +30,7 @@ def get_application(db_config: DBConfig) -> Application:
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["*"]
     )
 
     for r in ROUTERS:
