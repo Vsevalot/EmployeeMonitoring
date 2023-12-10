7fbe341abb2d5c1ea7421de45eb90e599892425c
Sokolov Max <maxerdan@yandex.ru>
2023-12-10 18:09:09 +0500

    Fixes

--- mobile/app/src/main/java/com/example/statohealth/infrastructure/Network.kt
+++ mobile/app/src/main/java/com/example/statohealth/infrastructure/Network.kt
@@ -53,6 +53,58 @@ class Network(val context: Context) {
         queue.add(request)
     }
 
+    inline fun <reified T : Any> sendPutRequest(
+        endpoint: String,
+        data: Any,
+        crossinline successResponseAction: (T?) -> Unit,
+        useAuth: Boolean = true
+    ) {
+        val jsonObject = JSONObject(Gson().toJson(data))
+        val request = object : JsonObjectRequest(
+            Method.PUT, getUrl(endpoint), jsonObject,
+            { responseJson ->
+                var response: T? = null
+                if (responseJson != null) {
+                    response =
+                        Gson().fromJson(responseJson.toString(), T::class.java)
+                }
+                successResponseAction(response)
+            }, { volleyError ->
+                val messageData = String(volleyError.networkResponse.data)
+                Logger.log("VolleyError: $messageData")
+            }) {
+
+            override fun parseNetworkResponse(response: NetworkResponse): Response<JSONObject?>? {
+                return try {
+                    val json = String(
+                        response.data
+                    )
+                    if (json.isEmpty()) {
+                        Response.success(
+                            null,
+                            HttpHeaderParser.parseCacheHeaders(response)
+                        )
+                    } else {
+                        Response.success(
+                            JSONObject(json),
+                            HttpHeaderParser.parseCacheHeaders(response)
+                        )
+                    }
+                } catch (e: UnsupportedEncodingException) {
+                    Response.error(ParseError(e))
+                }
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                if (useAuth)
+                    headers["authorization"] = "$authorizationToken"
+                return headers
+            }
+        }
+        queue.add(request)
+    }
+
     inline fun <reified T : Any> sendPostRequest(
         endpoint: String,
         data: Any,
@@ -111,6 +163,16 @@ class Network(val context: Context) {
     fun getUrl(endpoint: String): String = apiUrl + apiVersion + endpoint
 
     fun handleError(volleyError: VolleyError, endpoint: String) {
+        if(volleyError.message != null && volleyError.message!!.contains("Failed to connect to", ignoreCase = true))
+        {
+            AlertDialog.Builder(context)
+                .setTitle("Ошибка")
+                .setMessage("Нет связи с сервером")
+                .setPositiveButton("Ок") { _, _ -> }
+                .show()
+            Logger.log("VolleyError: Нет связи с сервером")
+            return
+        }
         val statusCode = volleyError.networkResponse.statusCode
         val messageData = String(volleyError.networkResponse.data)
         val message = "Code: $statusCode\n\n$messageData\n\nOn: $endpoint"
