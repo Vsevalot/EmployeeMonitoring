90f0c63a6a8118900584544eaf85fd40637d7cc8
Sokolov Max <maxerdan@yandex.ru>
2023-12-14 20:03:31 +0500

    Fix

--- mobile/app/src/main/java/com/example/statohealth/infrastructure/CurrentDate.kt
+++ mobile/app/src/main/java/com/example/statohealth/infrastructure/CurrentDate.kt
@@ -6,7 +6,7 @@ class CurrentDate {
     val now = Calendar.getInstance()
     val year = now.get(Calendar.YEAR)
 
-    val month = now.get(Calendar.MONTH)
+    val month = now.get(Calendar.MONTH)+1
     val day = now.get(Calendar.DAY_OF_MONTH)
 
     val hour = now.get(Calendar.HOUR_OF_DAY)
